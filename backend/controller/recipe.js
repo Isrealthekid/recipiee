487cfba0cb5f15d1a566994f762687713b3cb741
isrealthekid <oluwoleisreal12@gmail.com>
2024-10-02 18:02:37 +0100

    backendSetup

--- backend/controller/recipe.js
+++ backend/controller/recipe.js
@@ -1,5 +1,48 @@
-const getRecipe = (req,res)=>{
-    res.json({message: "Welcome to our recipe application"})
+const Recipes=require("../models/recipe")
+
+const getRecipes =async (req,res)=>{
+    const recipes = await Recipes.find()
+    return res.json(recipes)
 }
 
-module.exports = {getRecipe}
+const getRecipe = async(req,res)=>{
+    const recipe =await Recipes.findById(req.params.id)
+    res.json(recipe)
+
+}
+
+const addRecipe = async(req,res)=>{
+    const {title,ingredients,instructions,time}=req.body
+
+    if(!title || !ingredients || !instructions ){
+        return res.status(400).json({message: "Required fields cannot be empty"})
+    }
+
+    const newRecipe =await Recipes.create({
+        title,
+        ingredients,
+        instructions,
+        time
+    })
+    return res.json(newRecipe)
+}
+
+const editRecipe =async (req,res)=>{
+    const {title,ingredients,instructions,time}=req.body 
+    let recipe = await Recipes.findById(req.params.id)
+    try {
+        if(recipe){
+            await Recipes.findByIdAndUpdate(req.params.id,req.body,{new:true})
+            res.json({title,ingredients,instructions,time}) 
+        }
+    } catch (error) {
+        // console.log(error)
+        return res.status(404).json({message: "Recipe not found"})
+    }
+}
+
+const deleteRecipe = (req,res)=>{
+    res.json({message: "delete a recipe"})
+}
+
+module.exports = {getRecipes,getRecipe,addRecipe,editRecipe,deleteRecipe}
--- backend/routes/recipe.js
+++ backend/routes/recipe.js
@@ -1,7 +1,12 @@
 const express=require("express")
-const { getRecipe} = require("..//controller/recipe")
+const { getRecipes,getRecipe,addRecipe,editRecipe,deleteRecipe} = require("../controller/recipe")
 const router=express.Router()
 
-router.get("/",getRecipe)
+router.get("/",getRecipes)//get all recipes
+router.get("/:id",getRecipe)//get single recipe by id
+router.post("/",addRecipe)//add/create a new recipe
+router.put("/:id",editRecipe)//edit/update a recipe
+router.delete("/:id",deleteRecipe)//delete a recipe
+
 
 module.exports=router
