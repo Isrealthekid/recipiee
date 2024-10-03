eb685e286f4efde87428d271ab0d3a686c7be5e0
isrealthekid <oluwoleisreal12@gmail.com>
2024-10-03 17:36:40 +0100

    HomePage

--- frontend/recipy/src/App.jsx
+++ frontend/recipy/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './App.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import Home from './pages/Home'
+
+const router = createBrowserRouter([
+  {path: '/', element: <Home/>}
+])
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <RouterProvider router={router}></RouterProvider>
+    </>
   )
 }
 
