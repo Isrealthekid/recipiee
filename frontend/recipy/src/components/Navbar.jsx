2de5403f80f60c417c2fe3136d146099584f3e7b
isrealthekid <oluwoleisreal12@gmail.com>
2024-10-03 17:38:09 +0100

    NavBar

--- frontend/recipy/src/components/Navbar.jsx
+++ frontend/recipy/src/components/Navbar.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const Navbar = () => {
   return (
    <>
-    
+    <header>
+        <h2>Food Blog</h2>
+        <ul>
+            <li>Home</li>
+            <li>My Recipes</li>
+            <li>Favourites</li>
+            <li>Login</li>
+        </ul>
+    </header>
    </>
   )
 }
