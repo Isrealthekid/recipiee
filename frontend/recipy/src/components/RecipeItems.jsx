766362f2d661840d0d638b1c6adb7844d677289a
isrealthekid <oluwoleisreal12@gmail.com>
2024-10-03 20:22:10 +0100

    NewUserSignUp

--- frontend/recipy/src/components/RecipeItems.jsx
+++ frontend/recipy/src/components/RecipeItems.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import { useLoaderData } from 'react-router-dom'
+import foodImg from '../assets/foodRecipe.png'
+import { BiSolidStopwatch } from "react-icons/bi";
+import { BiSolidHeart } from "react-icons/bi";
 
 const RecipeItems = () => {
   const allRecipes=useLoaderData()
   console.log(allRecipes)
   return (
-    <div>RecipeItems</div>
+    <>
+    <div className='card-container'>
+      { allRecipes?.map((item, index) => {
+        return(
+          <div className='card' key={index}>
+            <img src={foodImg} width="120px" height="100px"></img>
+
+            <div className='card-body'>
+                <div className="title">{item.title}</div>
+              <div className="icons">
+                  <div className='time'><BiSolidStopwatch />{item.time}min</div>
+                  <BiSolidHeart />
+              </div>
+            </div>
+          </div>
+        )
+      })}
+    </div>
+    
+    </>
   )
 }
 
